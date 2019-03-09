8a63264451e4e9ff65e91862982b7d98460ed83c
YixinLin294 <l294681046@gmail.com>
2019-03-09 11:35:59 +0800

    add markdown support

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+from markdown import markdown
+import bleach
 
 
 class Permission:
@@ -212,6 +214,13 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
+        tags=allowed_tags, strip=True))
 
     @staticmethod
     def generate_fake(count=100):
@@ -227,4 +236,6 @@ class Post(db.Model):
             author=u)
         db.session.add(p)
         db.session.commit()
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
