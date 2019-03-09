a1ad0b4ec4a852e91a2c6171f4c7e9b69ffa337d
YixinLin294 <l294681046@gmail.com>
2019-03-09 23:46:55 +0800

    add comment-disable function

--- app/main/forms.py
+++ app/main/forms.py
@@ -53,3 +53,7 @@ class EditProfileAdminForm(FlaskForm):
 class PostForm(FlaskForm):
     body = PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    body = StringField('', validators=[Required()])
+    submit = SubmitField('Submit')
