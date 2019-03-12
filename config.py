f8d03650417d7e241e33c06b2b933fbe4fada029
YixinLin294 <l294681046@gmail.com>
2019-03-12 14:36:40 +0800

    add smtphandle ssl support

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     FLASKY_SLOW_DB_QUERY_TIME = 0.5
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.163.com'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 465
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') or True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <l294681046@163.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <yixin294681046@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
@@ -37,6 +37,8 @@ class TestingConfig(Config):
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 class ProductionConfig(Config):
+    # DEBUG = True
+    SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     @classmethod
@@ -45,18 +47,23 @@ class ProductionConfig(Config):
 
         # email errors to the administrators
         import logging
-        from logging.handlers import SMTPHandler
+        # from logging.handlers import SMTPHandler
+        from ssl_smpt import CompatibleSMTPSSLHandler
         credentials = None
         secure = None
+        is_use_ssl = None
         if getattr(cls, 'MAIL_USERNAME', None) is not None:
             credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-            if getattr(cls, 'MAIL_USE_SSL', None):
+            if getattr(cls, 'MAIL_USE_TLS', None):
                 secure = ()
-        mail_handler = SMTPHandler(
+            if getattr(cls, 'MAIL_USE_SSL', None):
+                is_use_ssl = True
+        mail_handler = CompatibleSMTPSSLHandler(
             mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
             fromaddr=cls.FLASKY_MAIL_SENDER,
             toaddrs=[cls.FLASKY_ADMIN],
             subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error', 
+            is_use_ssl=is_use_ssl,
             credentials=credentials,
             secure=secure)
         mail_handler.setLevel(logging.ERROR)
@@ -87,3 +94,16 @@ config = {
 
     'default': DevelopmentConfig
 }
+
+#  gmail:
+#     MAIL_SERVER = 'smtp.googlemail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_SSL = False
+#     MAIL_USE_TLS = True
+
+#  mail163:
+#     MAIL_SERVER = 'smtp.163.com'
+#     MAIL_PORT = 465
+#     MAIL_USE_SSL = True
+#     MAIL_USE_TLS = False
+
