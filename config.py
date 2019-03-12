3eab7f7493acdd17ed82edf009588901156a0b1a
YixinLin294 <l294681046@gmail.com>
2019-03-12 22:44:48 +0800

    fix 163 email spam problem:

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <yixin294681046@163.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <l294681046@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
