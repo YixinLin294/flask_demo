6140fe275feef05977306225f63ba7e8d61ba4dd
YixinLin294 <l294681046@gmail.com>
2019-03-12 15:43:24 +0800

    change

--- config.py
+++ config.py
@@ -8,10 +8,15 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     FLASKY_SLOW_DB_QUERY_TIME = 0.5
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.163.com'
-    MAIL_PORT = os.environ.get('MAIL_PORT') or 465
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') or True
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.163.com'
+    # MAIL_PORT = os.environ.get('MAIL_PORT') or 465
+    # MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') or True
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_SSL = False
+    MAIL_USE_TLS = True
+
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
